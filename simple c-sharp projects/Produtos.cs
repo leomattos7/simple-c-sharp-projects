8df4eaa02c14e15e532fe47aed50a994b6696665
Leonardo Mattos <ldmattos@inf.ufsm.br>
2023-09-12 00:50:20 -0300

    Adiciona exercicio sobre métodos

---
+++ simple c-sharp projects/Produtos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace produtos
+{
+    class Produtos
+    {
+        public string Nome { get; private set; } = "";
+        public double Preco;
+        public int Quantidade;
+
+        public double valorTotalEmEstoque()
+        {
+            return (Preco * Quantidade);
+        }
+        public Produtos cadastrarProdutos()
+        {
+            Console.WriteLine("Digite o nome do produto");
+            Nome = Console.ReadLine();
+            Console.WriteLine("Digite o preco ");
+            Preco = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade");
+            Quantidade = Convert.ToInt32(Console.ReadLine());
+            return this;
+        }
+        public void adicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void removerProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+    }
+}
--- simple c-sharp projects/main.cs
+++ simple c-sharp projects/main.cs
@@ -1,15 +1,35 @@
-﻿using System;
+﻿using produtos;
+using System;
 
 namespace simple_c_sharp_projects
 {
     class Program
     {
+        static void exibeInformações(Produtos p1)
+        {
+            double valor = p1.valorTotalEmEstoque();
+            Console.WriteLine("Dados do produto: " + p1.Nome);
+            Console.WriteLine("Valor: R$ " + p1.Preco);
+            Console.WriteLine("Quantidade: " + p1.Quantidade);
+            Console.WriteLine("Valor total: R$ " + valor);
+        }
         static void Main(string[] args)
         {
             /*TriangleArea areaTriangulo = new TriangleArea();
-            areaTriangulo.calculaAreaTriangulo();*/
+            areaTriangulo.calculaAreaTriangulo();
             calculaSalário informações = new calculaSalário();
-            informações.calculaSalario();
+            informações.calculaSalario*/
+            Produtos p1 = new Produtos();
+            int quantidade;
+            p1 = p1.cadastrarProdutos();
+            Console.WriteLine("Digite a quantidade de produtos que deseja adicionar: ");
+            quantidade = Convert.ToInt32(Console.ReadLine());
+            p1.adicionarProdutos(quantidade);
+            exibeInformações(p1);
+            Console.WriteLine("Digite a quantidade de produtos que deseja remover");
+            quantidade = Convert.ToInt32(Console.ReadLine());
+            p1.removerProdutos(quantidade);
+            exibeInformações(p1);
         }
     }
 }
